88faf3edc3bdcbac2760bfdc4b89d0867860f207
Aadarsh Malviya <malviyaaadarsh45@gmail.com>
2026-02-18 15:04:53 +0530

    updated app.py to include dynamic port assignment for deployment environments

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import os
 from src.ui.interface import app
 
 if __name__ == "__main__":
-    app.launch()
+    port = int(os.environ.get("PORT", 10000))
+
+    app.launch(server_name="0.0.0.0", server_port=port)
